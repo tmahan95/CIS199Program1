cfb888bfacce83f711b482a71798c0e115509411
tmahan95 <tmahan95@users.noreply.github.com>
2017-11-20 13:09:49 -0500

    Initial commit
    This program calculates the cost of a paint job based on the number of gallons required, the area to paint, and the number of coats.

---
+++ Prog1/Prog1/Prog1Form.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Prog1
+{
+    partial class Prog1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqFtInLbl = new System.Windows.Forms.Label();
+            this.coatsLbl = new System.Windows.Forms.Label();
+            this.sqFtInTxt = new System.Windows.Forms.TextBox();
+            this.coatsTxt = new System.Windows.Forms.TextBox();
+            this.ppgTxt = new System.Windows.Forms.TextBox();
+            this.ppgLbl = new System.Windows.Forms.Label();
+            this.hoursLbl = new System.Windows.Forms.Label();
+            this.gallonsLbl = new System.Windows.Forms.Label();
+            this.TotSqFtLbl = new System.Windows.Forms.Label();
+            this.laborCostLbl = new System.Windows.Forms.Label();
+            this.paintCostLbl = new System.Windows.Forms.Label();
+            this.totalCostLbl = new System.Windows.Forms.Label();
+            this.totalCostOutLbl = new System.Windows.Forms.Label();
+            this.laborCostOutLbl = new System.Windows.Forms.Label();
+            this.paintCostOutLbl = new System.Windows.Forms.Label();
+            this.hoursOutLbl = new System.Windows.Forms.Label();
+            this.gallonsOutLbl = new System.Windows.Forms.Label();
+            this.totalSqFtOutLbl = new System.Windows.Forms.Label();
+            this.inputHeadingLbl = new System.Windows.Forms.Label();
+            this.outputHeadingLbl = new System.Windows.Forms.Label();
+            this.quoteBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sqFtInLbl
+            // 
+            this.sqFtInLbl.AutoSize = true;
+            this.sqFtInLbl.Location = new System.Drawing.Point(1, 61);
+            this.sqFtInLbl.Name = "sqFtInLbl";
+            this.sqFtInLbl.Size = new System.Drawing.Size(130, 13);
+            this.sqFtInLbl.TabIndex = 0;
+            this.sqFtInLbl.Text = "Square feet of wall space:";
+            // 
+            // coatsLbl
+            // 
+            this.coatsLbl.AutoSize = true;
+            this.coatsLbl.Location = new System.Drawing.Point(16, 88);
+            this.coatsLbl.Name = "coatsLbl";
+            this.coatsLbl.Size = new System.Drawing.Size(115, 13);
+            this.coatsLbl.TabIndex = 1;
+            this.coatsLbl.Text = "Coats of paint to apply:";
+            // 
+            // sqFtInTxt
+            // 
+            this.sqFtInTxt.Location = new System.Drawing.Point(133, 58);
+            this.sqFtInTxt.Name = "sqFtInTxt";
+            this.sqFtInTxt.Size = new System.Drawing.Size(65, 20);
+            this.sqFtInTxt.TabIndex = 2;
+            // 
+            // coatsTxt
+            // 
+            this.coatsTxt.Location = new System.Drawing.Point(133, 84);
+            this.coatsTxt.Name = "coatsTxt";
+            this.coatsTxt.Size = new System.Drawing.Size(65, 20);
+            this.coatsTxt.TabIndex = 3;
+            // 
+            // ppgTxt
+            // 
+            this.ppgTxt.Location = new System.Drawing.Point(133, 110);
+            this.ppgTxt.Name = "ppgTxt";
+            this.ppgTxt.Size = new System.Drawing.Size(65, 20);
+            this.ppgTxt.TabIndex = 4;
+            // 
+            // ppgLbl
+            // 
+            this.ppgLbl.AutoSize = true;
+            this.ppgLbl.Location = new System.Drawing.Point(48, 113);
+            this.ppgLbl.Name = "ppgLbl";
+            this.ppgLbl.Size = new System.Drawing.Size(83, 13);
+            this.ppgLbl.TabIndex = 5;
+            this.ppgLbl.Text = "Price per gallon:";
+            // 
+            // hoursLbl
+            // 
+            this.hoursLbl.AutoSize = true;
+            this.hoursLbl.Location = new System.Drawing.Point(246, 113);
+            this.hoursLbl.Name = "hoursLbl";
+            this.hoursLbl.Size = new System.Drawing.Size(76, 13);
+            this.hoursLbl.TabIndex = 8;
+            this.hoursLbl.Text = "Hours of labor:";
+            // 
+            // gallonsLbl
+            // 
+            this.gallonsLbl.AutoSize = true;
+            this.gallonsLbl.Location = new System.Drawing.Point(238, 87);
+            this.gallonsLbl.Name = "gallonsLbl";
+            this.gallonsLbl.Size = new System.Drawing.Size(84, 13);
+            this.gallonsLbl.TabIndex = 7;
+            this.gallonsLbl.Text = "Gallons needed:";
+            // 
+            // TotSqFtLbl
+            // 
+            this.TotSqFtLbl.AutoSize = true;
+            this.TotSqFtLbl.Location = new System.Drawing.Point(232, 61);
+            this.TotSqFtLbl.Name = "TotSqFtLbl";
+            this.TotSqFtLbl.Size = new System.Drawing.Size(90, 13);
+            this.TotSqFtLbl.TabIndex = 6;
+            this.TotSqFtLbl.Text = "Total square feet:";
+            // 
+            // laborCostLbl
+            // 
+            this.laborCostLbl.AutoSize = true;
+            this.laborCostLbl.Location = new System.Drawing.Point(262, 189);
+            this.laborCostLbl.Name = "laborCostLbl";
+            this.laborCostLbl.Size = new System.Drawing.Size(60, 13);
+            this.laborCostLbl.TabIndex = 10;
+            this.laborCostLbl.Text = "Labor cost:";
+            // 
+            // paintCostLbl
+            // 
+            this.paintCostLbl.AutoSize = true;
+            this.paintCostLbl.Location = new System.Drawing.Point(265, 163);
+            this.paintCostLbl.Name = "paintCostLbl";
+            this.paintCostLbl.Size = new System.Drawing.Size(57, 13);
+            this.paintCostLbl.TabIndex = 9;
+            this.paintCostLbl.Text = "Paint cost:";
+            // 
+            // totalCostLbl
+            // 
+            this.totalCostLbl.AutoSize = true;
+            this.totalCostLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCostLbl.Location = new System.Drawing.Point(254, 219);
+            this.totalCostLbl.Name = "totalCostLbl";
+            this.totalCostLbl.Size = new System.Drawing.Size(68, 13);
+            this.totalCostLbl.TabIndex = 11;
+            this.totalCostLbl.Text = "Total cost:";
+            // 
+            // totalCostOutLbl
+            // 
+            this.totalCostOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalCostOutLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCostOutLbl.Location = new System.Drawing.Point(323, 219);
+            this.totalCostOutLbl.Name = "totalCostOutLbl";
+            this.totalCostOutLbl.Size = new System.Drawing.Size(69, 15);
+            this.totalCostOutLbl.TabIndex = 17;
+            this.totalCostOutLbl.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // laborCostOutLbl
+            // 
+            this.laborCostOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.laborCostOutLbl.Location = new System.Drawing.Point(323, 189);
+            this.laborCostOutLbl.Name = "laborCostOutLbl";
+            this.laborCostOutLbl.Size = new System.Drawing.Size(69, 15);
+            this.laborCostOutLbl.TabIndex = 16;
+            this.laborCostOutLbl.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // paintCostOutLbl
+            // 
+            this.paintCostOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paintCostOutLbl.Location = new System.Drawing.Point(323, 163);
+            this.paintCostOutLbl.Name = "paintCostOutLbl";
+            this.paintCostOutLbl.Size = new System.Drawing.Size(69, 15);
+            this.paintCostOutLbl.TabIndex = 15;
+            this.paintCostOutLbl.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // hoursOutLbl
+            // 
+            this.hoursOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hoursOutLbl.Location = new System.Drawing.Point(323, 113);
+            this.hoursOutLbl.Name = "hoursOutLbl";
+            this.hoursOutLbl.Size = new System.Drawing.Size(69, 15);
+            this.hoursOutLbl.TabIndex = 14;
+            // 
+            // gallonsOutLbl
+            // 
+            this.gallonsOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gallonsOutLbl.Location = new System.Drawing.Point(323, 87);
+            this.gallonsOutLbl.Name = "gallonsOutLbl";
+            this.gallonsOutLbl.Size = new System.Drawing.Size(69, 15);
+            this.gallonsOutLbl.TabIndex = 13;
+            // 
+            // totalSqFtOutLbl
+            // 
+            this.totalSqFtOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalSqFtOutLbl.Location = new System.Drawing.Point(323, 61);
+            this.totalSqFtOutLbl.Name = "totalSqFtOutLbl";
+            this.totalSqFtOutLbl.Size = new System.Drawing.Size(69, 15);
+            this.totalSqFtOutLbl.TabIndex = 12;
+            // 
+            // inputHeadingLbl
+            // 
+            this.inputHeadingLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputHeadingLbl.Location = new System.Drawing.Point(4, 19);
+            this.inputHeadingLbl.Name = "inputHeadingLbl";
+            this.inputHeadingLbl.Size = new System.Drawing.Size(194, 20);
+            this.inputHeadingLbl.TabIndex = 18;
+            this.inputHeadingLbl.Text = "Enter Customer Data";
+            this.inputHeadingLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // outputHeadingLbl
+            // 
+            this.outputHeadingLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputHeadingLbl.Location = new System.Drawing.Point(231, 19);
+            this.outputHeadingLbl.Name = "outputHeadingLbl";
+            this.outputHeadingLbl.Size = new System.Drawing.Size(161, 20);
+            this.outputHeadingLbl.TabIndex = 19;
+            this.outputHeadingLbl.Text = "Paint Job Quote";
+            this.outputHeadingLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // quoteBtn
+            // 
+            this.quoteBtn.Location = new System.Drawing.Point(37, 160);
+            this.quoteBtn.Name = "quoteBtn";
+            this.quoteBtn.Size = new System.Drawing.Size(108, 23);
+            this.quoteBtn.TabIndex = 20;
+            this.quoteBtn.Text = "Generate Quote";
+            this.quoteBtn.UseVisualStyleBackColor = true;
+            this.quoteBtn.Click += new System.EventHandler(this.quoteBtn_Click);
+            // 
+            // Prog1Form
+            // 
+            this.AcceptButton = this.quoteBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 245);
+            this.Controls.Add(this.quoteBtn);
+            this.Controls.Add(this.outputHeadingLbl);
+            this.Controls.Add(this.inputHeadingLbl);
+            this.Controls.Add(this.totalCostOutLbl);
+            this.Controls.Add(this.laborCostOutLbl);
+            this.Controls.Add(this.paintCostOutLbl);
+            this.Controls.Add(this.hoursOutLbl);
+            this.Controls.Add(this.gallonsOutLbl);
+            this.Controls.Add(this.totalSqFtOutLbl);
+            this.Controls.Add(this.totalCostLbl);
+            this.Controls.Add(this.laborCostLbl);
+            this.Controls.Add(this.paintCostLbl);
+            this.Controls.Add(this.hoursLbl);
+            this.Controls.Add(this.gallonsLbl);
+            this.Controls.Add(this.TotSqFtLbl);
+            this.Controls.Add(this.ppgLbl);
+            this.Controls.Add(this.ppgTxt);
+            this.Controls.Add(this.coatsTxt);
+            this.Controls.Add(this.sqFtInTxt);
+            this.Controls.Add(this.coatsLbl);
+            this.Controls.Add(this.sqFtInLbl);
+            this.Name = "Prog1Form";
+            this.Text = "Program 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sqFtInLbl;
+        private System.Windows.Forms.Label coatsLbl;
+        private System.Windows.Forms.TextBox sqFtInTxt;
+        private System.Windows.Forms.TextBox coatsTxt;
+        private System.Windows.Forms.TextBox ppgTxt;
+        private System.Windows.Forms.Label ppgLbl;
+        private System.Windows.Forms.Label hoursLbl;
+        private System.Windows.Forms.Label gallonsLbl;
+        private System.Windows.Forms.Label TotSqFtLbl;
+        private System.Windows.Forms.Label laborCostLbl;
+        private System.Windows.Forms.Label paintCostLbl;
+        private System.Windows.Forms.Label totalCostLbl;
+        private System.Windows.Forms.Label totalCostOutLbl;
+        private System.Windows.Forms.Label laborCostOutLbl;
+        private System.Windows.Forms.Label paintCostOutLbl;
+        private System.Windows.Forms.Label hoursOutLbl;
+        private System.Windows.Forms.Label gallonsOutLbl;
+        private System.Windows.Forms.Label totalSqFtOutLbl;
+        private System.Windows.Forms.Label inputHeadingLbl;
+        private System.Windows.Forms.Label outputHeadingLbl;
+        private System.Windows.Forms.Button quoteBtn;
+    }
+}
+
---
+++ Prog1/Prog1/Prog1Form.cs
@@ -0,0 +1,70 @@
+﻿// Program 1
+// CIS 199-01/-75
+// Due: 2/14/2016
+// By: Andrew L. Wright
+
+// This program calculates a quote for a painting job
+// based on data entered by the user
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog1
+{
+    public partial class Prog1Form : Form
+    {
+        public Prog1Form()
+        {
+            InitializeComponent();
+        }
+
+        private void quoteBtn_Click(object sender, EventArgs e)
+        {
+            // Constants
+            const double SQ_FEET_PER_GAL = 325.0; // Square feet covered per gallon of paint
+            const double HOURS_PER_GAL = 8.0;     // Hours of labor per gallon of paint
+            const double LABOR_HR_RATE = 10.50;   // Hourly pay rate for labor
+
+            // Input variables
+            double sqFt;     // Square feet to be painted
+            byte coats;      // Number of coats of paint
+            double galPrice; // Price of gallon of paint
+
+            // Output variables
+            double totalSqFt;  // Total square feet, including coats
+            uint gallons;      // Whole number of gallons of paint needed
+            double hoursLabor; // Hours of labor needed
+            double paintCost;  // Cost of paint needed
+            double laborCost;  // Cost of labor needed
+            double totalCost;  // Total cost of job
+
+            // Parse input data
+            sqFt = double.Parse(sqFtInTxt.Text);
+            coats = byte.Parse(coatsTxt.Text);
+            galPrice = double.Parse(ppgTxt.Text);
+
+            // Perform paint job calculations
+            totalSqFt = sqFt * coats;
+            gallons = (uint)Math.Ceiling(totalSqFt / SQ_FEET_PER_GAL); // Round up, cast as whole number
+            hoursLabor = totalSqFt / SQ_FEET_PER_GAL * HOURS_PER_GAL;
+            paintCost = gallons * galPrice;
+            laborCost = hoursLabor * LABOR_HR_RATE;
+            totalCost = paintCost + laborCost;
+
+            // Display results
+            totalSqFtOutLbl.Text = totalSqFt.ToString("N1");
+            gallonsOutLbl.Text = gallons.ToString(); // Could also use format code "D"
+            hoursOutLbl.Text = hoursLabor.ToString("N1");
+            paintCostOutLbl.Text = paintCost.ToString("C");
+            laborCostOutLbl.Text = laborCost.ToString("C");
+            totalCostOutLbl.Text = totalCost.ToString("C");
+        }
+    }
+}
